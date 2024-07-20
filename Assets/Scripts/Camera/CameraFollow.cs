cdb11a720811ac699afeea75f7e2fa139838965e
Алексей Макаров <33080157+theokunb@users.noreply.github.com>
2024-07-20 17:02:24 +0900

    сделал слежение камеры

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using Zenject;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Inject] private IMapService _mapService;
+    [Inject] private Player _player;
+
+    private float _cameraWidth;
+    private float _cameraHeight;
+
+    private void Start()
+    {
+        var camera = Camera.main;
+
+        _cameraHeight = 2f * camera.orthographicSize;
+        _cameraWidth = _cameraHeight * camera.aspect;
+    }
+
+    private void FixedUpdate()
+    {
+        var minX = _mapService.MapBounds.Point1.x + _cameraWidth / 2f;
+        var maxX = _mapService.MapBounds.Point2.x - _cameraWidth / 2f;
+        var minZ = _mapService.MapBounds.Point1.z + _cameraHeight / 2f;
+        var maxZ = _mapService.MapBounds.Point3.z - _cameraHeight / 2f;
+
+        float targetX = _player.transform.position.x;
+        float targetZ = _player.transform.position.z;
+
+        if(_player.transform.position.x < minX)
+        {
+            targetX = minX;
+        }
+        else if(_player.transform.position.x > maxX)
+        {
+            targetX = maxX;
+        }
+
+        if(_player.transform.position.z < minZ)
+        {
+            targetZ = minZ;
+        }
+        else if (_player.transform.position.z > maxZ)
+        {
+            targetZ = maxZ;
+        }
+
+        transform.position = new Vector3(targetX, transform.position.y, targetZ);
+    }
+}
--- Assets/Scripts/TestInstaller.cs
+++ Assets/Scripts/TestInstaller.cs
@@ -3,13 +3,17 @@ using Zenject;
 
 public class TestInstaller : MonoInstaller
 {
+    [SerializeField] private Player _player;
     [SerializeField] private MainHand _mainHand;
     [SerializeField] private WeaponHold _weaponHold;
+    [SerializeField] private TerrainMap _terrainMap;
 
     public override void InstallBindings()
     {
         Container.Bind<MainHand>().FromInstance(_mainHand);
         Container.Bind<IWeaponVisitor>().FromInstance(_weaponHold);
         Container.Bind<WeaponService>().AsSingle();
+        Container.Bind<IMapService>().FromInstance(_terrainMap);
+        Container.Bind<Player>().FromInstance(_player);
     }
 }
